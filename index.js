c02ca1215e25c2cc4b3774a06741abcbfb1d57a2
tom <thomcdixon@gmail.com>
2018-12-11 05:46:29 -0800

    CORS test ready

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 
+'use strict';
+
+const googleGeocode = 'https://maps.googleapis.com/maps/api/geocode/json?';
+const googleApiKey = 'AIzaSyDVx0Obu2xJ6E8SCGESOFbetaVXMKDQwMA';
+const ebirdApiKey = '3k3ndtikp21v';
+
+
+function getEbird(latitude, longitude) {
+  const ebirdNearbyUrl = `https://ebird.org/ws2.0/data/obs/geo/recent?key=3k3ndtikp21v&lat=${latitude}&lng=${longitude}&sort=date&maxResults=20`;
+
+  fetch(ebirdNearbyUrl)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      console.log(jsonResponse);
+    });
+}
+
+function geocodeAddress(address) {
+  // GET latitude and longitude from location
+
+  const queryParams = `address=${encodeURIComponent(address)}`;
+
+  const searchString = googleGeocode + queryParams + `&key=${googleApiKey}`;
+
+  fetch(searchString)
+    .then(response => response.json())
+    .then(json => {
+      const lat = json.results[0].geometry.location.lat;
+      const lng = json.results[0].geometry.location.lng;
+      console.log(lat, lng); // 
+
+      getEbird(lat, lng);
+    });
+}
+
 
 function handleSubmit() {
   $('form').on('click', 'input#submit-button', event => {
     event.preventDefault();
-    console.log($('.user-input').val())
-  })
-}
+
+    const address = $('.user-input').val();
+
+    geocodeAddress(address);
+  });
+};
 
 
-function autocomplete() {
-  $('.user-input').autocomplete({
-    minLength: 3,
-    source: renamedKeys
-  })
 
-
-  // $('form').on('keyup', '.user-input', event => {
-
-
-  //   // // // user text 
-  //   const textContent = $('.user-input').val();
-  //   console.log(textContent);
-
-  //   const testPattern = new RegExp(textContent, "i");
-
-  //   const result = trimmedSpecies.find(species => {
-  //     return testPattern.test(species.commonName);
-  //   })
-
-  //   console.log(result);
-  // })
-
-}
-
-
-$(autocomplete)
 $(handleSubmit)
