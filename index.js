eb10125870b036f71aaaf3daefb1beb466eacaf3
tom <thomcdixon@gmail.com>
2018-12-16 21:56:49 -0800

    finish toggle markers

--- index.js
+++ index.js
@@ -1,54 +1,69 @@
 
 'use strict';
 
+let map;
 let zIndex = 0;
+let markers = [];
+
 const googleGeocode = 'https://maps.googleapis.com/maps/api/geocode/json?';
 const ebirdNearbyUrlBase = 'https://ebird.org/ws2.0/data/obs/geo/recent?key=3k3ndtikp21v&sort=date&';
 
 const googleApiKey = 'AIzaSyDVx0Obu2xJ6E8SCGESOFbetaVXMKDQwMA';
 const ebirdApiKey = '3k3ndtikp21v';
 
-let eBirdReturnObj = {};
+function addMarkerToArr(location, label, eBirdData) {
 
+  map.panTo(location);
 
-function handleMapButtonClick(map, eBirdData) {
-  // listen for map button click
-  $('#js-results-list').on('click', '.map-button', function (event) {
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    label: label,
+    zIndex: zIndex
+  });
 
-    const lat = parseFloat(event.target.getAttribute("lat"));
-    const lng = parseFloat(event.target.getAttribute("lng"));
+  zIndex++;
 
-    const label = $(event.target).parent()[0].childNodes[0].data.split(' ')[0];
-
-    const latLng = { lat: lat, lng: lng };
-
-    map.setCenter(latLng);
-    const marker = new google.maps.Marker({
-      position: latLng,
-      map: map,
-      label: label
-    });
-
-    marker.setZIndex(parseInt(zIndex));
-
-    zIndex++;
-
-    const infoWindowContent = `
+  const infoWindowContent = `
       <h3><a href="https://www.allaboutbirds.org/search/?q=${eBirdData[label - 1].comName}" target="_blank">${eBirdData[label - 1].comName}</a><h3>
       <p>Location: ${eBirdData[label - 1].locName}</p>
       <p>Date: ${eBirdData[label - 1].obsDt}</li>
       `;
 
-    marker.addListener('click', function () {
-      new google.maps.InfoWindow({
-        content: infoWindowContent
-      }).open(map, marker);
-    });
+  marker.addListener('click', function () {
+    new google.maps.InfoWindow({ content: infoWindowContent })
+      .open(map, marker);
   });
+
+  markers.push(marker);
 }
 
+function handleMapButtonClick(eBirdData) {
+  $('#js-results-list').on('click', '.map-button', function (event) {
 
+    const label = $(this).parent()[0].childNodes[0].data.split(' ')[0];
 
+    let controlFlow = true;
+
+    markers.forEach(function (marker, index) {
+      if (marker.label === label) {
+        markers[index].setMap(null);
+        markers.splice(index, 1);
+        controlFlow = false;
+      }
+    });
+
+    if (controlFlow) {
+      const lat = parseFloat(this.getAttribute("lat"));
+      const lng = parseFloat(this.getAttribute("lng"));
+
+      const latLng = { lat: lat, lng: lng };
+
+      addMarkerToArr(latLng, label, eBirdData);
+    }
+
+  });
+}
 
 function renderObservations(responseJson) {
   // remove previous results
@@ -59,13 +74,12 @@ function renderObservations(responseJson) {
     $('#js-results-list').append(
       `<li>${counter} ${obs.comName} | <button class="map-button" lat=${obs.lat} lng=${obs.lng}>Map</button></li>`
     );
-
     counter++;
   }
 };
 
 function getEbirdData(latitude, longitude, maxResults) {
-  console.log('getEbirdData run')
+
   let ebirdNearbyUrl = ebirdNearbyUrlBase + `lat=${latitude}&lng=${longitude}`;
 
   if (maxResults) { ebirdNearbyUrl += `&maxResults=${maxResults}`; }
@@ -75,14 +89,12 @@ function getEbirdData(latitude, longitude, maxResults) {
     .then(jsonResponse => {
       console.log('ebird fetch', jsonResponse);
 
-      // generate info window
-      eBirdReturnObj = jsonResponse;
       renderObservations(jsonResponse);
-      // generateInfoWindowContent(jsonResponse);
-      const map = initMap();
 
-      map.setCenter(new google.maps.LatLng(latitude, longitude));
+      // centers map
+      map.panTo(new google.maps.LatLng(latitude, longitude));
 
+      // draw search radius
       new google.maps.Circle({
         strokeColor: '#FF0000',
         strokeOpacity: 0.6,
@@ -92,14 +104,14 @@ function getEbirdData(latitude, longitude, maxResults) {
         map: map,
         center: { lat: latitude, lng: longitude },
         radius: 25000
-      })
+      });
 
-      handleMapButtonClick(map, jsonResponse);
+      handleMapButtonClick(jsonResponse);
     });
 }
 
 function geocodeAddress(location) {
-  console.log('geocodeAddress run')
+
   const queryParams = `address=${encodeURIComponent(location)}`;
 
   const searchString = googleGeocode + queryParams + `&key=${googleApiKey}`;
@@ -110,34 +122,33 @@ function geocodeAddress(location) {
       const lat = json.results[0].geometry.location.lat;
       const lng = json.results[0].geometry.location.lng;
       const maxResults = $('#max-results').val();
-      console.log('max results: ', maxResults)
+
       getEbirdData(lat, lng, maxResults);
     });
 }
 
 
-function handleSubmit() {
+function handleLocationSubmit() {
   $('form').on('click', 'input#submit-button', event => {
+    markers = [];
     event.preventDefault();
-
-    console.log('handle submit');
 
     const location = $('.user-input').val();
 
     geocodeAddress(location);
-  });
-};
 
+  });
+}
 
 function initMap() {
-  console.log('init map run')
-  const map = new google.maps.Map(document.querySelector('.map'), {
+  map = new google.maps.Map(document.querySelector('.map'), {
+
     center: { lat: 40.7828647, lng: -73.9653551 },
     zoom: 10
   });
 
-  return map;
+  console.log(map);
 }
 
 $(initMap);
-$(handleSubmit);
+$(handleLocationSubmit);
